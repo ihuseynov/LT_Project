7fafe23867660f460d4dc65a095785108ff58752
ihuseynov <ihuseynov2021@ada.edu.az>
2022-09-08 14:39:22 +0200

    committing project

---
+++ BiLSTM_CRF.py
@@ -0,0 +1,126 @@
+import torch
+import torch.nn as nn
+from utils import *
+from torch.nn import init
+
+START_TAG = "<START>"
+STOP_TAG = "<STOP>"
+
+
+def log_sum(smat):
+    max_score = smat.max(dim=0, keepdim=True).values
+    return (smat - max_score).exp().sum(axis=0, keepdim=True).log() + max_score
+
+
+class BiLSTM_CRF(nn.Module):
+    def __init__(self, vocab_size, tag_to_ix, embedding_dim,
+                 hidden_dim, char_lstm_dim=25, char_to_ix=None,
+                 pre_word_embeds=None, char_embedding_dim=25,):
+
+        super(BiLSTM_CRF, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+        self.vocab_size = vocab_size
+        self.tag_to_ix = tag_to_ix
+        self.tagset_size = len(tag_to_ix)
+        self.out_channels = char_lstm_dim
+
+        print("out_channels: %d, hidden_dim: %d, " % ( char_lstm_dim, hidden_dim))
+
+        if char_embedding_dim is not None:
+            self.char_lstm_dim = char_lstm_dim
+            self.char_embeds = nn.Embedding(len(char_to_ix), char_embedding_dim)
+            torch.nn.init.xavier_uniform_(self.char_embeds.weight)
+            self.char_cnn3 = nn.Conv2d(in_channels=1, out_channels=self.out_channels, kernel_size=(3, char_embedding_dim), padding=(2, 0),)
+
+        self.word_embeds = nn.Embedding(vocab_size, embedding_dim)
+        if pre_word_embeds is not None:
+            self.pre_word_embeds = True
+            self.word_embeds.weight = nn.Parameter(torch.FloatTensor(pre_word_embeds))
+        else:
+            self.pre_word_embeds = False
+
+        self.dropout = nn.Dropout(0.5)
+
+        self.lstm = nn.LSTM(embedding_dim + self.out_channels, hidden_dim, bidirectional=True)
+
+        for param in self.lstm.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+
+        self.hidden2tag = nn.Linear(hidden_dim * 2, self.tagset_size)
+        init.xavier_normal_(self.hidden2tag.weight.data)
+        init.normal_(self.hidden2tag.bias.data)
+
+        self.transitions = nn.Parameter(torch.randn(self.tagset_size, self.tagset_size))
+        self.transitions.data[:, tag_to_ix[START_TAG]] = -10000
+        self.transitions.data[tag_to_ix[STOP_TAG], :] = -10000
+
+    def _get_lstm_features(self, sentence, chars, caps):
+
+        chars_embeds = self.char_embeds(chars).unsqueeze(1)
+        chars_cnn_out3 = self.char_cnn3(chars_embeds)
+        chars_embeds = nn.functional.max_pool2d(chars_cnn_out3,kernel_size=(chars_cnn_out3.size(2), 1)).view(chars_cnn_out3.size(0), self.out_channels)
+
+        embeds = self.word_embeds(sentence)
+
+        embeds = torch.cat((embeds, chars_embeds), 1)
+
+        embeds = embeds.unsqueeze(1)
+        embeds = self.dropout(embeds)  # dropout
+        lstm_out, _ = self.lstm(embeds)
+        lstm_out = lstm_out.view(len(sentence), self.hidden_dim * 2)
+        lstm_out = self.dropout(lstm_out)  # dropout
+        lstm_feats = self.hidden2tag(lstm_out)
+        return lstm_feats
+
+    def _score_sentence(self, feats, tags):
+
+        r = torch.LongTensor(range(feats.size()[0]))
+        pad_start_tags = torch.cat([torch.LongTensor([self.tag_to_ix[START_TAG]]), tags])
+        pad_stop_tags = torch.cat([tags, torch.LongTensor([self.tag_to_ix[STOP_TAG]])])
+
+        score = torch.sum(self.transitions[pad_start_tags, pad_stop_tags]) + torch.sum(feats[r, tags])
+        return score
+
+    def _forward_alg(self, feats):
+
+        alpha = torch.full((1, self.tagset_size), -10000.0)
+
+        alpha[0][self.tag_to_ix[START_TAG]] = 0.0
+
+        for feat in feats:
+            alpha = log_sum(alpha.T + feat.unsqueeze(0) + self.transitions)
+        return log_sum(alpha.T + 0 + self.transitions[:, [self.tag_to_ix[STOP_TAG]]]).flatten()[0]
+
+    def neg_log_likelihood(self, sentence, tags, chars, caps):
+
+        features = self._get_lstm_features(sentence, chars, caps)
+
+        forward_score = self._forward_alg(features)
+        gold_score = self._score_sentence(features, tags)
+        return forward_score - gold_score
+
+    def forward(self, sentence, chars, caps):
+        features = self._get_lstm_features(sentence, chars, caps)
+        backtrace = []
+
+        alpha = torch.full((1, self.tagset_size), -10000.0)
+        alpha[0][self.tag_to_ix[START_TAG]] = 0
+
+        for feat in features:
+            smat = (alpha.T + feat.unsqueeze(0) + self.transitions)  # (tagset_size, tagset_size)
+        backtrace.append(smat.argmax(0))  # column_max
+        alpha = log_sum(smat)
+
+        smat = alpha.T + 0 + self.transitions[:, [self.tag_to_ix[STOP_TAG]]]
+        best_tag_id = smat.flatten().argmax().item()
+        best_path = [best_tag_id]
+
+        for bptrs_t in reversed(backtrace[1:]):
+            best_tag_id = bptrs_t[best_tag_id].item()
+        best_path.append(best_tag_id)
+
+        return log_sum(smat).item(), best_path[::-1]
---
+++ utils.py
@@ -0,0 +1,182 @@
+import os
+import re
+import BiLSTM_CRF
+from collections import Counter
+
+models_path = "./models"
+eval_path = "./evaluation"
+eval_temp = os.path.join(eval_path, "temp")
+eval_script = os.path.join(eval_path, "conlleval")
+
+
+def create_mapping(dico):
+
+    sorted_items = sorted(dico.items(), key=lambda x: (-x[1], x[0]))
+    id_to_item = {i: v[0] for i, v in enumerate(sorted_items)}
+    item_to_id = {v: k for k, v in id_to_item.items()}
+    return item_to_id, id_to_item
+
+
+def iob_to_iobes(tags):
+    new_tags = []
+    for i, tag in enumerate(tags):
+        if tag == "O":
+            new_tags.append(tag)
+        elif tag.split("-")[0] == "B":
+            if i + 1 != len(tags) and tags[i + 1].split("-")[0] == "I":
+                new_tags.append(tag)
+            else:
+                new_tags.append(tag.replace("B-", "S-"))
+        elif tag.split("-")[0] == "I":
+            if i + 1 < len(tags) and tags[i + 1].split("-")[0] == "I":
+                new_tags.append(tag)
+            else:
+                new_tags.append(tag.replace("I-", "E-"))
+        else:
+            raise Exception("Invalid IOB format!")
+    return new_tags
+
+
+def load_sentences(path, zeros):
+    sentences = []
+    sentence = []
+    for line in open(path, 'r', encoding='utf-8'):
+        line = re.sub("\d", "0", line.rstrip()) if zeros else line.rstrip()
+
+        if not line and len(sentence) > 0:
+
+            if 'DOCSTART' not in sentence[0][0]:
+                sentences.append(sentence)
+            sentence = []
+
+        else:
+            word = line.split()
+            assert len(word) >= 2
+            sentence.append(word)
+
+    if len(sentence) > 0:
+        if 'DOCSTART' not in sentence[0][0]:
+            sentences.append(sentence)
+    return sentences
+
+
+def iob2(tags):
+    for i, tag in enumerate(tags):
+        if tag == "O":
+            continue
+        split = tag.split("-")
+        if len(split) != 2 or split[0] not in ["I", "B"]:
+            return False
+        if split[0] == "B":
+            continue
+        elif i == 0 or tags[i - 1] == "O":  # conversion IOB1 to IOB2
+            tags[i] = "B" + tag[1:]
+        elif tags[i - 1][1:] == tag[1:]:
+            continue
+        else:  # conversion IOB1 to IOB2
+            tags[i] = "B" + tag[1:]
+    return True
+
+
+def update_tag_scheme(sentences, tag_scheme):
+
+    for i, s in enumerate(sentences):
+        tags = [w[-1] for w in s]
+
+        if not iob2(tags):
+            continue
+
+        elif tag_scheme == 'iobes':
+            new_tags = iob_to_iobes(tags)
+            for word, new_tag in zip(s, new_tags):
+                word[-1] = new_tag
+
+
+def prepare_dataset(sentences, word_to_id, char_to_id, tag_to_id, lower=True):
+
+    def f(x): return x.lower() if lower else x
+    data = []
+    for sentence in sentences:
+        str_words = [w[0] for w in sentence]
+        words = [word_to_id[f(w) if f(w) in word_to_id else '<UNK>']
+                 for w in str_words]
+        # Skip characters that are not in the training set
+        chars = [[char_to_id[c if c in char_to_id else '<UNK>'] for c in w]
+                 for w in str_words]
+        caps = []
+
+        for s in str_words:
+            if s.lower() == s:
+                caps.append(0)
+            elif s.upper() == s:
+                caps.append(1)
+            elif s[0].upper() == s[0]:
+                caps.append(2)
+            else:
+                caps.append(3)
+
+    tags = [tag_to_id[w[-1]] for w in sentence]
+
+    data.append({'str_words': str_words, 'words': words, 'chars': chars, 'caps': caps, 'tags': tags, })
+    return data
+
+
+def mapping(type_mapping, sentences, lower=None):
+
+    if type_mapping == "tag":
+        tags = [word[-1] for s in sentences for word in s]
+        dico = dict(Counter(tags))
+        dico[BiLSTM_CRF.START_TAG] = -1
+        dico[BiLSTM_CRF.STOP_TAG] = -2
+        tag_to_id, id_to_tag = create_mapping(dico)
+        print("Found %i unique named entity tags" % len(dico))
+        return dico, tag_to_id, id_to_tag
+
+    elif type_mapping == "char":
+        chars = ''.join([w[0] for s in sentences for w in s])
+        dico = dict(Counter(chars))
+        dico['<PAD>'] = 10000001
+        dico['<UNK>'] = 10000000
+
+        char_to_id, id_to_char = create_mapping(dico)
+        print("Found %i unique characters" % len(dico))
+        return dico, char_to_id, id_to_char
+
+    elif type_mapping == "word":
+        words = [x[0].lower() if lower else x[0] for s in sentences for x in s]
+        dico = dict(Counter(words))
+        dico['<PAD>'] = 10000001
+        dico['<UNK>'] = 10000000
+        dico = {k: v for k, v in dico.items() if v >= 3}
+        word_to_id, id_to_word = create_mapping(dico)
+
+        print("Found %i unique words (%i in total)" % (len(dico), len(words)))
+        return dico, word_to_id, id_to_word
+
+
+def augment_with_pretrained(dictionary, ext_emb_path, words):
+    print('Loading pretrained embeddings from %s...' % ext_emb_path)
+    assert os.path.isfile(ext_emb_path)
+
+    pretrained = set([
+        line.rstrip().split()[0].strip()
+        for line in open(ext_emb_path, 'r', encoding='utf-8')
+    ])
+
+    if words is None:
+        for word in pretrained:
+            if word not in dictionary:
+                dictionary[word] = 0
+    else:
+        for word in words:
+            if any(x in pretrained for x in [
+                word,
+                word.lower(),
+                re.sub('\d', '0', word.lower())
+            ]) and word not in dictionary:
+                dictionary[word] = 0
+
+    word_to_id, id_to_word = create_mapping(dictionary)
+    return dictionary, word_to_id, id_to_word
+
+
